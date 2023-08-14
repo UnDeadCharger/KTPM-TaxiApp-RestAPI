663323c1cecccdb0b82925e12a76580a368bbb46
Hoang Minh <hoangminhbadao@gmail.com>
2023-08-14 21:04:55 +0700

    intiali commit

--- RestAPI/src/main.ts
+++ RestAPI/src/main.ts
@@ -1,22 +1,56 @@
-import { NestFactory } from '@nestjs/core';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
+// import { NestFactory } from '@nestjs/core';
+// import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+// import { AppModule } from './app.module';
+// import { ValidationPipe } from '@nestjs/common';
 
+// async function bootstrap() {
+//   const app = await NestFactory.create(AppModule);
+//   //Intercept package to validate input
+//   app.useGlobalPipes(new ValidationPipe());
+
+//   const config = new DocumentBuilder()
+//     .setTitle('Median')
+//     .setDescription('The Median API')
+//     .setVersion('0.1')
+//     .build();
+
+//   const document = SwaggerModule.createDocument(app, config);
+//   SwaggerModule.setup('api', app, document);
+
+//   await app.listen(3000);
+// }
+// bootstrap();
+
+
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { ConfigService } from '@nestjs/config';
+ 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  //Intercept package to validate input
-  app.useGlobalPipes(new ValidationPipe());
+  const configService = app.get(ConfigService);
+ 
+  const user = configService.get('RABBITMQ_USER');
+  const password = configService.get('RABBITMQ_PASSWORD');
+  const host = configService.get('RABBITMQ_HOST');
+  const queueName = configService.get('RABBITMQ_QUEUE_NAME');
+ 
+  
 
-  const config = new DocumentBuilder()
-    .setTitle('Median')
-    .setDescription('The Median API')
-    .setVersion('0.1')
-    .build();
+  await app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.RMQ,
+    options: {
+      urls: [`amqp://${user}:${password}@${host}`],
+      queue: queueName,
+      queueOptions: {
+        durable: true,
+      },
+    },
+  });
+ 
+  app.startAllMicroservices();
 
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
-
-  await app.listen(3000);
+  
 }
 bootstrap();
