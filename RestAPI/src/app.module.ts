8a529612371a07824d741c050349d0e2478fb300
UnDeadCharger <rexfury121@gmail.com>
2023-08-14 21:16:22 +0700

    Moving some file

--- RestAPI/src/app.module.ts
+++ RestAPI/src/app.module.ts
@@ -6,11 +6,18 @@ import { KhachHangsModule } from './khach-hangs/khach-hangs.module';
 import { ChuyenXeModule } from './chuyen-xe/chuyen-xe.module';
 import { TaiXeModule } from './tai-xe/tai-xe.module';
 import { XeModule } from './xe/xe.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [PrismaModule, KhachHangsModule, ChuyenXeModule, TaiXeModule, XeModule],
+  imports: [
+    PrismaModule,
+    KhachHangsModule,
+    ChuyenXeModule,
+    TaiXeModule,
+    XeModule,
+    ConfigModule.forRoot(),
+  ],
   controllers: [AppController],
   providers: [AppService],
-  
 })
 export class AppModule {}
--- RestAPI/src/main.ts
+++ RestAPI/src/main.ts
@@ -2,11 +2,19 @@ import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   //Intercept package to validate input
+  const configService = app.get(ConfigService);
   app.useGlobalPipes(new ValidationPipe());
+
+  const user = configService.get('RABBITMQ_USER');
+  const password = configService.get('RABBITMQ_PASSWORD');
+  const host = configService.get('RABBITMQ_HOST');
+  const queueName = configService.get('RABBITMQ_QUEUE_NAME');
 
   const config = new DocumentBuilder()
     .setTitle('Median')
@@ -16,6 +24,20 @@ async function bootstrap() {
 
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
+
+  //Microservice
+  await app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.RMQ,
+    options: {
+      urls: [`amqp://${user}:${password}@${host}`],
+      queue: queueName,
+      queueOptions: {
+        durable: true,
+      },
+    },
+  });
+
+  app.startAllMicroservices();
 
   await app.listen(3000);
 }
