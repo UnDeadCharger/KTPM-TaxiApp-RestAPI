a01f45374a933bf09aff4dfdc92343510b04c2c1
Hoang Minh <hoangminhbadao@gmail.com>
2023-08-16 21:53:23 +0700

    Consumer Implementation for Trip Tracker

--- trip-tracker/src/app.controller.ts
+++ trip-tracker/src/app.controller.ts
@@ -1,10 +1,39 @@
+// import { Controller, Get } from '@nestjs/common';
+// import { AppService } from './app.service';
+
+// @Controller()
+// export class AppController {
+//   constructor(private readonly appService: AppService) {}
+
+//   @Get()
+//   getHello(): string {
+//     return this.appService.getHello();
+//   }
+// }
+
 import { Controller, Get } from '@nestjs/common';
+import {
+  MessagePattern,
+  RmqContext,
+  Ctx,
+  Payload,
+} from '@nestjs/microservices';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  @MessagePattern('rabbit-mq-producer')
+  public async execute(@Payload() data: any, @Ctx() context: RmqContext) {
+    const channel = context.getChannelRef();
+    const originalMessage = context.getMessage();
+    console.log('data', data);
+    await this.appService.mySuperLongProcessOfUser(data);
+    channel.ack(originalMessage);
+  }
+
+  // You can also include your getHello() method here if needed
   @Get()
   getHello(): string {
     return this.appService.getHello();
