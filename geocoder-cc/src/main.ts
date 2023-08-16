d546e64c11be5d92c07125e9683bfe66af0c0422
Hoang Minh <hoangminhbadao@gmail.com>
2023-08-16 22:28:20 +0700

    Consumer Implementation for Geocoder-CC

--- geocoder-cc/src/main.ts
+++ geocoder-cc/src/main.ts
@@ -1,8 +1,37 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
+  // Create the NestJS application
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  // Swagger configuration
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('Median')
+    .setDescription('The Median API description')
+    .setVersion('0.1')
+    .build();
+
+  // Create Swagger document and set up Swagger UI
+  const swaggerDocument = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('api', app, swaggerDocument);
+
+  // Microservice configuration
+  const microservice = await NestFactory.createMicroservice(AppModule, {
+    transport: Transport.RMQ,
+    options: {
+      urls: ['amqp://admin:admin@localhost:5672'],
+      queue: 'CC-TripTracker-subscribers',
+      noAck: false,
+      prefetchCount: 1,
+    },
+  });
+
+  // Start the NestJS application and the microservice
+  await Promise.all([app.listen(3002), microservice.listen()]);
+
+  console.log('Application and microservice are up and running.');
 }
 bootstrap();
